1916285bb5a224e53ff434bf3bfcb3803d52cc59
zmm29 <andre.all3@hotmail.com>
2023-02-06 16:47:27 -0300

    Sobrecarga de metodos

---
+++ Metodos/Sobrecarga/src/Main.java
@@ -0,0 +1,13 @@
+import javax.sound.sampled.SourceDataLine;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Exercicio quadrilatero");
+
+        Quadrilatero.area(3);
+        Quadrilatero.area(7, 8, 9);
+        Quadrilatero.area(5d, 5d);
+        Quadrilatero.area(5f, 3f);
+    }
+    
+}
