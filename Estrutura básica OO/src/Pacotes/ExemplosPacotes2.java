6665fd82921bd47256f6069b5884160873588369
zmm29 <andre.all3@hotmail.com>
2023-02-28 16:24:37 -0300

    Pacotes e visibilidade

---
+++ Estrutura básica OO/src/Pacotes/ExemplosPacotes2.java
@@ -0,0 +1,10 @@
+package Pacotes;
+
+import Associacao.*;
+
+public class ExemplosPacotes2 {
+    protected int andre = 1;
+    private double daniel = 2.0;
+    public String marta = "Marta"; 
+     // Tipos de visibilidade tambem
+}
